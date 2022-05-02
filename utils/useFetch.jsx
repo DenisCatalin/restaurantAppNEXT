3dcbeac772c7e6822c46b53358315d2adcc2def7
DenisCatalin <deniscotecata@gmail.com>
2022-05-02 23:35:41 +0300

    fix errors

--- utils/useFetch.jsx
+++ utils/useFetch.jsx
@@ -158,21 +158,6 @@ export async function useUploadPhoto(displayName, url, profilePic, username) {
   return data;
 }
 
-export async function useGetLikes(displayName, asset_id) {
-  const res = await fetch("/api/getLikes", {
-    method: "GET",
-    headers: {
-      body: JSON.stringify({
-        displayName: displayName,
-        photoId: asset_id,
-      }),
-    },
-  });
-  const data = await res.json();
-
-  return data;
-}
-
 export async function useGetComments(asset_id) {
   const res = await fetch("/api/getComments", {
     method: "GET",
