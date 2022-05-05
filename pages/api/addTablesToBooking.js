a5035cc67637090dfe84d25a718e8cbbe2f53b28
DenisCatalin <deniscotecata@gmail.com>
2022-05-05 15:15:50 +0300

    fix addtables

--- pages/api/addTablesToBooking.js
+++ pages/api/addTablesToBooking.js
@@ -7,9 +7,6 @@ import {
 
 export default async function BookTables(req, res) {
   if (req.method === "POST") {
-    const check = await checkReservation(token, date);
-    const exist = check?.data?.booking?.length;
-    const value = check?.data?.booking[0]?.bookingTables;
     try {
       const token = req ? req.cookies?.token : null;
       const tables = req ? JSON.parse(req.headers.body).tables : null;
@@ -18,51 +15,59 @@ export default async function BookTables(req, res) {
       const date = req ? JSON.parse(req.headers.body).date : null;
       const currentDate = req ? JSON.parse(req.headers.body).currentDate : null;
 
-      if (exist === 0) {
-        const defaultValue = "0|0|0|0|0|0|0|0|0";
-        const array2 = defaultValue.split("|");
-        tables.forEach((i) => {
-          array2[i - 1] = 1;
-        });
-        const addBooking = await addReservation(
-          token,
-          array2.toString().replaceAll(",", "|"),
-          date,
-          currentDate
-        );
+      const check = await checkReservation(token, date);
 
-        const addBookingForUser = await addToBookingHistory(
-          token,
-          issuer,
-          seats,
-          currentDate,
-          date,
-          tables.toString()
-        );
+      if (check) {
+        const exist = check?.data?.booking?.length;
+        const value = check?.data?.booking[0]?.bookingTables;
+        if (exist === 0) {
+          const defaultValue = "0|0|0|0|0|0|0|0|0";
+          const array2 = defaultValue.split("|");
+          tables.forEach((i) => {
+            array2[i - 1] = 1;
+          });
+          const addBooking = await addReservation(
+            token,
+            array2.toString().replaceAll(",", "|"),
+            date,
+            currentDate
+          );
 
-        res.send({ message: "Complete", addBooking, addBookingForUser });
+          const addBookingForUser = await addToBookingHistory(
+            token,
+            issuer,
+            seats,
+            currentDate,
+            date,
+            tables.toString()
+          );
+
+          res.send({ message: "Complete", addBooking, addBookingForUser });
+        } else {
+          const defaultValue = value;
+          const array2 = defaultValue.split("|");
+          tables.forEach((i) => {
+            array2[i - 1] = 1;
+          });
+          const modifyBooking = await modifyReservation(
+            token,
+            date,
+            array2.toString().replaceAll(",", "|")
+          );
+
+          const addBookingForUser = await addToBookingHistory(
+            token,
+            issuer,
+            seats,
+            currentDate,
+            date,
+            tables
+          );
+
+          res.send({ message: "Complete", modifyBooking, addBookingForUser });
+        }
       } else {
-        const defaultValue = value;
-        const array2 = defaultValue.split("|");
-        tables.forEach((i) => {
-          array2[i - 1] = 1;
-        });
-        const modifyBooking = await modifyReservation(
-          token,
-          date,
-          array2.toString().replaceAll(",", "|")
-        );
-
-        const addBookingForUser = await addToBookingHistory(
-          token,
-          issuer,
-          seats,
-          currentDate,
-          date,
-          tables
-        );
-
-        res.send({ message: "Complete", modifyBooking, addBookingForUser });
+        res.status(500).send({ message: "Incomplete", check: check });
       }
     } catch (error) {
       console.error("Something went wrong booking the tables", error);
